a00ee9aef3f15fb96ae75e1cc33a71ae125451ed
Julius Bacosa <juliusbacosa@gmail.com>
2018-10-10 01:54:21 +0800

    Add project files.

---
+++ ULTRALOTTOGENERATOR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ULTRALOTTOGENERATOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
